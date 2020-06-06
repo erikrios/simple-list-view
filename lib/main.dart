72b1b4b70c442ea794b1552753caf98ab35461a6
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-06 21:58:14 +0700

    Add ListView.builder and ListTile widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,23 @@ void main() => runApp(MaterialApp(
     ));
 
 class HomeApp extends StatelessWidget {
+  List<int> dummyTexts = List<int>.generate(1000, (index) => index + 1);
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: Text('Simple List View'),
-    ),
-  );
+        appBar: AppBar(
+          title: Text('Simple List View'),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text("Title ${dummyTexts[index]}"),
+              subtitle: Text("Subtitle for ${dummyTexts[index]}"),
+              leading: Icon(
+                  (index % 2 == 0) ? Icons.airport_shuttle : Icons.android),
+            );
+          },
+          itemCount: dummyTexts.length,
+        ),
+      );
 }
